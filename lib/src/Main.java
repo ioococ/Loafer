1c9767fadb23a680f1a0369f6c65d703360be1dc
aotusoft <1520351630@qq.com>
2023-05-09 23:46:31 +0800

    Upload Code

--- lib/src/Main.java
+++
@@ -1,9 +0,0 @@
-import com.ami.superfancyclouds.SuperFancyCloudsRenderer;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        SuperFancyCloudsRenderer sfc = new SuperFancyCloudsRenderer();
-        System.out.println(sfc.toString());
-    }
-}
---
+++ mvcGetStarted/src/main/java/org/mvcGetStarted/controller/HelloController.java
@@ -0,0 +1,12 @@
+package org.mvcGetStarted.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/")
+    public String indexPage(){
+        return "index";
+    }
+}
