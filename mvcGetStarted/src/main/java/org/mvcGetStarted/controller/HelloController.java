60690e6cfca22e902792f30830dbbccfd37cd4fa
aotusoft <1520351630@qq.com>
2023-05-10 16:11:32 +0800

    Upload Code

--- mvcGetStarted/src/main/java/org/mvcGetStarted/controller/HelloController.java
+++ mvcGetStarted/src/main/java/org/mvcGetStarted/controller/HelloController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-    @RequestMapping("/")
+    @RequestMapping({"/", "index"})
     public String indexPage(){
-        return "index";
+        return "/static/index";
+//        return "forward:/target";
+    }
+    @RequestMapping("/target")
+    public String toTarget(){
+        return "/static/target";
     }
 }
