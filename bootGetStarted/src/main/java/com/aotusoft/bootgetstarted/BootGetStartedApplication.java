9e7869eb9494a6a4cfb92930dba4d0e15cb4a484
aotusoft <1520351630@qq.com>
2023-05-11 21:23:28 +0800

    Upload Code

---
+++ bootGetStarted/src/main/java/com/aotusoft/bootgetstarted/BootGetStartedApplication.java
@@ -0,0 +1,13 @@
+package com.aotusoft.bootgetstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootGetStartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootGetStartedApplication.class, args);
+	}
+
+}
---
+++ bootGetStarted/src/test/java/com/aotusoft/bootgetstarted/BootGetStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aotusoft.bootgetstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootGetStartedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
