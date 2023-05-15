0c17cc2da6335ba05d165872b000324a9fe6af9b
aotusoft <1520351630@qq.com>
2023-05-15 16:07:20 +0800

    Upload files

--- webDep/src/main/java/com/aotusoft/web/controller/HelloController.java
+++ webDep/src/main/java/com/aotusoft/web/controller/HelloController.java
@@ -1,12 +1,19 @@
 package com.aotusoft.web.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//在templates目录下的所有页面，只能通过Controller来访问
+@Controller
 public class HelloController {
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World";
+//    @GetMapping("/hello")
+//    public String hello() {
+//        return "Hello World";
+//    }
+    @RequestMapping("/")
+    public String index(){
+        return "index.html";
     }
 }
