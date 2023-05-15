3994dc9151b4298b93dc173e7715de1441f078e6
aotusoft <1520351630@qq.com>
2023-05-15 11:24:26 +0800

    Upload files

---
+++ webDep/src/main/java/com/aotusoft/web/WebDepApplication.java
@@ -0,0 +1,13 @@
+package com.aotusoft.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebDepApplication.class, args);
+    }
+
+}
---
+++ webDep/src/test/java/com/aotusoft/web/WebDepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aotusoft.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
