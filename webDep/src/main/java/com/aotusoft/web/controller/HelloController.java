9c1befea9118f3ed6e38232a6d37a06075cdfda3
aotusoft <1520351630@qq.com>
2023-05-15 17:00:11 +0800

    Upload files

--- webDep/src/main/java/com/aotusoft/web/controller/HelloController.java
+++ webDep/src/main/java/com/aotusoft/web/controller/HelloController.java
@@ -1,9 +1,12 @@
 package com.aotusoft.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 //在templates目录下的所有页面，只能通过Controller来访问
 @Controller
@@ -16,5 +19,11 @@ public class HelloController {
     public String index(){
         //在不加入thymeleaf模板引擎的情况下需要加入后缀名
         return "index.html";
+    }
+    @RequestMapping("/test")
+    public String test(Model model){
+        model.addAttribute("msg","<p>This is a message</p>");
+        model.addAttribute("users", Arrays.asList("aotusoft","qingjiang","shuax","runningcheese"));
+        return "test";
     }
 }
---
+++ webDep/src/main/resources/public/TitleController.js
@@ -0,0 +1,15 @@
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function() {
+    if (document.hidden) {
+        document.title = '(つェ⊂)我藏好了哦~ '
+        clearTimeout(titleTime);
+    }
+    else {
+        document.title = '(*´∇｀*) 被你发现啦~ ' + OriginTitile;
+        titleTime = setTimeout(function() {
+            document.title = OriginTitile;
+        }, 2000);
+    }
+});
+
