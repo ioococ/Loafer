89d92b8010ca7812d29c852f6221e2bede7672b9
aotusoft <1520351630@qq.com>
2023-05-18 16:59:01 +0800

    Upload Code

--- webDep/src/main/java/com/aotusoft/web/config/MVConfig.java
+++ webDep/src/main/java/com/aotusoft/web/config/MVConfig.java
@@ -1,8 +1,18 @@
 package com.aotusoft.web.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+//@EnableWebMvc
+/*
+ * @EnableWebMvc注解导入了DelegatingWebMvcConfiguration类
+ * 而DelegatingWebMvcConfiguration类继承了WebMvcConfigurationSupport类
+ * 且WebMvcAutoConfiguration类中存在@ConditionalOnMissingBean({WebMvcConfigurationSupport.class})注解
+ * 所以当WebMvcConfigurationSupport存在时WebMvcAutoConfiguration将不会生效
+* */
 
 //扩展SpringMVC
 @Configuration
