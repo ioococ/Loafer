91e50ad115a595cd281ae8e4ee3970f8888482cb
aotusoft <1520351630@qq.com>
2023-05-19 17:16:10 +0800

    Upload Code

--- employeeManager/src/main/java/com/aotusoft/config/MVConfig.java
+++ employeeManager/src/main/java/com/aotusoft/config/MVConfig.java
@@ -1,10 +1,16 @@
-//package com.aotusoft.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class MVConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addV
-//}
+package com.aotusoft.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MVConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
+    }
+
+}
