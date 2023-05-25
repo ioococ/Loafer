b322028b9fa7df6e0b042e0b5dafa316e61f64d0
aotusoft <1520351630@qq.com>
2023-05-25 22:00:37 +0800

    Upload Code

--- employeeManager/src/main/java/com/aotusoft/controller/EmpsController.java
+++ employeeManager/src/main/java/com/aotusoft/controller/EmpsController.java
@@ -1,10 +1,13 @@
 package com.aotusoft.controller;
 
+import com.aotusoft.dao.DepartmentDao;
 import com.aotusoft.dao.EmployeeDao;
+import com.aotusoft.pojo.Department;
 import com.aotusoft.pojo.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Collection;
@@ -13,10 +16,21 @@ import java.util.Collection;
 public class EmpsController {
     @Autowired
     EmployeeDao employeeDao;
-    @RequestMapping({"/emps","/emps.html"})
-    public String listAll(Model model){
+    @Autowired
+    DepartmentDao departmentDao;
+
+    @RequestMapping({"/emps", "/emps.html"})
+    public String listAll(Model model) {
         Collection<Employee> allEmps = employeeDao.getAll();
-        model.addAttribute("emps",allEmps);
+        model.addAttribute("emps", allEmps);
         return "list";
+    }
+
+    @GetMapping("/emps")
+    @RequestMapping("/add")
+    public String toAdd(Model model) {
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments",departments);
+        return "add";
     }
 }
