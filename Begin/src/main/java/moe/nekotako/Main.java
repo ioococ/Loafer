468af28f0bfed1422d3931841c74ca0b66020e90
aotusoft <1520351630@qq.com>
2023-08-28 16:25:14 +0800

    buying

--- Begin/src/main/java/moe/nekotako/Main.java
+++ Begin/src/main/java/moe/nekotako/Main.java
@@ -13,12 +13,13 @@ public class Main {
         for (int i = 1; i < 6; i++) {
             System.out.println("欢迎光临第" + i + "家专卖店");
             while (true) {
-                System.out.print("要离开吗(y/n)");
-                boolean doing = input.nextLine().equals("y");
-
-                if (doing) {
+                System.out.print("要离开吗(y/n)：");
+                if (input.nextLine().equals("y")) {
                     System.out.println("离店结账");
+                    System.out.println();
                     break;
+                } else {
+                    System.out.println("买了一件衣服");
                 }
                 num++;
             }
