6bdf560f093115a10bcaa7165185f5b7d2112d05
aotusoft <51018049+aotusoft@users.noreply.github.com>
2023-09-21 03:39:42 +0000

    commit

--- Nonka/src/a/App.java
+++ Nonka/src/a/App.java
@@ -5,10 +5,21 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("中文测试");
+        // System.out.println("Hello, World!");
+        // System.out.println("中文测试");
         List<Integer> list = new ArrayList<>();
         list.add(1);
-        System.out.println(list);
+        String s = "我的世界";
+        char c = '\u7682';
+        // for (int i = 0; i < s.length(); i++) {
+        // System.out.println(s.charAt(i));
+        // }
+        // s = (String) c;
+        // s = String.valueOf(c);
+        byte b = (byte) 128;
+        // System.out.println((float) (0.1 + 0.2));
+        System.out.println(b);
+        int i = c;
+
     }
 }
