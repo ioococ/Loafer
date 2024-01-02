86210125a01e2bdccda6316e2eb8f48338d82d87
ioococ <cheerupi@outlook.com>
2024-01-02 19:43:58 +0800

    init empty branch

--- Shopping/src/main/java/ink/onei/shopping/service/IOrderService.java
+++
@@ -1,8 +0,0 @@
-package ink.onei.shopping.service;
-
-import ink.onei.shopping.vo.OrderDetailVO;
-import org.apache.ibatis.annotations.Param;
-
-public interface IOrderService {
-    OrderDetailVO getOrderDetail(Integer id);
-}
