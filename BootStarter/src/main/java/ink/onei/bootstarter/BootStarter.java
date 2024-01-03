4587771b3aef4cb00c6224866bcae404019524a9
ioococ <cheerupi@outlook.com>
2024-01-03 09:09:10 +0800

    spring boot start

---
+++ BootStarter/src/main/java/ink/onei/bootstarter/BootStarter.java
@@ -0,0 +1,17 @@
+package ink.onei.bootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: nekotako
+ * @Description: TODO
+ * @Date: 02/01/2024 20:00 Tuesday
+ */
+
+@SpringBootApplication
+public class BootStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(BootStarter.class);
+    }
+}
---
+++ BootStarter/src/main/java/ink/onei/bootstarter/controller/HelloController.java
@@ -0,0 +1,14 @@
+package ink.onei.bootstarter.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Author: nekotako
+ * @Description: TODO
+ * @Date: 03/01/2024 09:00 Wednesday
+ */
+
+@Controller
+public class HelloController {
+
+}
