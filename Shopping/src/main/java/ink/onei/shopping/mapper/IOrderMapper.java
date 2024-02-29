ba0cf7f67b235735c698b95f0c26d53815afe917
ioococ <cheerupi@outlook.com>
2024-02-29 10:03:20 +0800

    init new branch

--- Shopping/src/main/java/ink/onei/shopping/mapper/IOrderMapper.java
+++
@@ -1,8 +0,0 @@
-package ink.onei.shopping.mapper;
-
-import ink.onei.shopping.vo.OrderDetailVO;
-import org.apache.ibatis.annotations.Param;
-
-public interface IOrderMapper {
-    OrderDetailVO getOrderDetail(@Param("id") Integer id);
-}
--- Shopping/src/main/java/ink/onei/shopping/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package ink.onei.shopping.service;
-
-import ink.onei.shopping.vo.UserDetailVO;
-
-import java.util.List;
-
-public interface IUserService {
-    List<UserDetailVO> getUserDetails(Integer pageNum, Integer pageSize);
-}
