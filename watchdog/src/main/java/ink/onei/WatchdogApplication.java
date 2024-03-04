1ccec59a2c98984a5eeb5532e607414715fc58b9
ioococ <cheerupi@outlook.com>
2024-03-04 16:53:10 +0800

    add spring gateway

---
+++ watchdog/src/main/java/ink/onei/WatchdogApplication.java
@@ -0,0 +1,16 @@
+package ink.onei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: nekotako
+ * @Description: TODO
+ * @Date: 04/03/2024 14:44 Monday
+ */
+@SpringBootApplication
+public class WatchdogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WatchdogApplication.class, args);
+    }
+}
